d88372241976a5a5f17315dcd26d70e476da38b4
wptechprodigy <waheed.freelancer16@gmail.com>
2020-05-26 16:20:35 +0100

    setup endpoint to delete a message

--- server/src/main.ts
+++ server/src/main.ts
@@ -13,33 +13,78 @@ app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 // Dealling with CORS issues and allowing access to the API
 app.use((_inRequest: Request, inResponse: Response, inNext: NextFunction) => {
-  inResponse.header('Access-Control-Allow-Origin', '*');
-  inResponse.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
-  inResponse.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept');
+	inResponse.header('Access-Control-Allow-Origin', '*');
+	inResponse.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
+	inResponse.header(
+		'Access-Control-Allow-Headers',
+		'Origin,X-Requested-With,Content-Type,Accept',
+	);
 
-  inNext();
+	inNext();
 });
 
 // GET: retrieves all mailboxes
 app.get('/mailboxes', async (_inRequest: Request, inResponse: Response) => {
-  try {
-    const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
-    const mailboxes: IMAP.IMailBox[] = await imapWorker.listMailBoxes();
-    inResponse.json(mailboxes);
-  } catch (inError) {
-    inResponse.send('error');
-  }
+	try {
+		const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+		const mailboxes: IMAP.IMailBox[] = await imapWorker.listMailBoxes();
+
+		inResponse.json(mailboxes);
+	} catch (inError) {
+		inResponse.send('error');
+	}
 });
 
 // GET: retrieves a particular mailbox
-app.get('/mailboxes/:mailbox', async (inRequest: Request, inResponse: Response) => {
-  try {
-    const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
-    const messages: IMAP.IMessage[] = await imapWorker.listMessages({
-      mailbox: inRequest.params.mailbox
-    });
-    inResponse.json(messages);
-  } catch (inError) {
-    inResponse.send('error');
-  }
-});
+app.get(
+	'/mailboxes/:mailbox',
+	async (inRequest: Request, inResponse: Response) => {
+		try {
+			const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+			const messages: IMAP.IMessage[] = await imapWorker.listMessages({
+				mailbox: inRequest.params.mailbox,
+			});
+
+			inResponse.json(messages);
+		} catch (inError) {
+			inResponse.send('error');
+		}
+	},
+);
+
+// GET: retrieve a particular message from a mailbox
+app.get(
+	'/messages/:mailbox/:id',
+	async (inRequest: Request, inResponse: Response) => {
+		try {
+			const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+			const message: IMAP.IMessage = await imapWorker.getMessageBody({
+				mailbox: inRequest.params.mailbox,
+				id: parseInt(inRequest.params.id, 10),
+			});
+
+			// Message body sent as plain-text
+			inResponse.send(message);
+		} catch (inError) {
+			inResponse.send('error');
+		}
+	},
+);
+
+// DELETE: delete a message
+app.delete(
+	'/messages/:mailbox/:id',
+	async (inRequest: Request, inResponse: Response) => {
+		try {
+			const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+			await imapWorker.deleteMessage({
+				mailbox: inRequest.params.mailbox,
+				id: parseInt(inRequest.params.id, 10),
+			});
+
+			inResponse.send('ok!');
+		} catch (inError) {
+			inResponse.send('error');
+		}
+	},
+);
