bacd62126d97feb4e2d8345a48ef3f98f64ab41c
wptechprodigy <waheed.freelancer16@gmail.com>
2020-05-26 15:47:23 +0100

    setup basic express app

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,1 +1,21 @@
-console.log('Hello world by Waheed');
+import path from 'path';
+import express, { Express, NextFunction, Request, Response } from 'express';
+import { serverInfo } from './ServerInfo';
+import * as IMAP from './IMAP';
+import * as SMTP from './SMTP';
+import * as Contacts from './Contacts';
+import { IContacts } from './Contacts';
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../../client/dist')));
+
+// Dealling with CORS issues and allowing access to the API
+app.use((inRequest: Request, inResponse: Response, inNext: NextFunction) => {
+  inResponse.header('Access-Control-Allow-Origin', '*');
+  inResponse.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
+  inResponse.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept');
+
+  inNext();
+});
